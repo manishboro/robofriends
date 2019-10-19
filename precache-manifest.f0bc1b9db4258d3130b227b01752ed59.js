7d80a7d7b24cf8d6b5df4fe164697758d5637cd3
manishboro <officialmanishboro@gmail.com>
2019-10-19 16:01:07 +0530

    Updates

---
+++ precache-manifest.f0bc1b9db4258d3130b227b01752ed59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc9fdf129db1814da2390de1ac18358",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c7d4ff27fd06eecfa825",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "c7d4ff27fd06eecfa825",
+    "url": "/robofriends/static/js/main.2945785c.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
